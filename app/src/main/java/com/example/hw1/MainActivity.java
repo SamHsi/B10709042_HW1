f155d0e59110ed18f85b1630742ff110699d3bbd
l3339 <l3339911@gmail.com>
2020-04-18 04:12:55 +0800

    test

--- app/src/main/java/com/example/hw1/MainActivity.java
+++ app/src/main/java/com/example/hw1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.hw1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClick(View v){
         
     }
 }
